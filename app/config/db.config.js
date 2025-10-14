6f571f547557ddbe96917bcf1d3d157590c3e05a
volkan.ulutas <volkan.ulutas@esensi.com.tr>
2025-10-14 14:11:17 +0300

    init

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -8,6 +8,12 @@ db.sequelize = new Sequelize(process.env.DATABASE_URL, {
   protocol: "postgres",
   dialectOptions: {
     ssl: { require: true, rejectUnauthorized: false }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
 });
 
